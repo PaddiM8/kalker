024a215a412b9f6d457ec5d68ab58db207d07b6a
PaddiM8 <paddim8@pm.me>
2020-06-05 17:06:24 +0200

    Removed trailing zeroes from results.

--- kalk_cli/src/output.rs
+++ kalk_cli/src/output.rs
@@ -4,13 +4,35 @@ use kalk::parser::{self};
 pub fn eval(parser: &mut parser::Context, input: &str) {
     match parser::parse(parser, input, 53) {
         Ok(Some(result)) => {
-            if result.clone().fract() == 0 {
+            if result.is_infinite() {
+                err("Too big to process.");
+            } else if result > 100_000_000 || result < -100_000_00 {
+                let sign = if result >= 0 { "" } else { "-" };
+                let (_, digits, exp) = result.to_sign_string_exp(10, None);
+                let value = format!(
+                    "{}.{}",
+                    digits[0..1].to_string(),
+                    digits[1..].to_string().trim_end_matches('0')
+                );
+
+                println!("{}{}*10^{}", sign, value, exp.unwrap() - 1);
+            } else if result.clone().fract() == 0 {
                 println!("{}", result.to_integer().unwrap());
             } else {
-                println!("{}", result);
+                println!(
+                    "{}",
+                    result
+                        .to_string()
+                        .trim_end_matches('0')
+                        .trim_end_matches('.')
+                );
             }
         }
         Ok(None) => print!(""),
-        Err(err) => println!("{}", Red.paint(err)),
+        Err(msg) => err(&msg),
     }
+}
+
+fn err(msg: &str) {
+    println!("{}", Red.paint(msg));
 }
