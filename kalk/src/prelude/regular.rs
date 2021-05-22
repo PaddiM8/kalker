637c3e331b7502208ad682dd3f69bd0afd62d2bc
bakk <bakk@tuta.io>
2021-05-22 20:47:10 +0200

    Implemented complex variants of prelude functions

--- kalk/src/prelude/regular.rs
+++ kalk/src/prelude/regular.rs
@@ -1,211 +1,34 @@
-use super::*;
-use crate::ast::Expr;
-use crate::interpreter;
-
-// Unary functions
-pub struct UnaryFuncInfo(pub(super) fn(f64) -> f64, pub(super) FuncType);
-
-pub struct BinaryFuncInfo(pub(super) fn(f64, f64) -> f64, pub(super) FuncType);
-
-impl UnaryFuncInfo {
-    fn call(&self, context: &mut interpreter::Context, x: f64, angle_unit: &str) -> f64 {
-        let func = self.0;
-        match self.1 {
-            FuncType::Trig => func(from_angle_unit(context, x, angle_unit)),
-            FuncType::InverseTrig => to_angle_unit(context, func(x), angle_unit),
-            FuncType::Other => func(x),
-        }
-    }
-}
-
-impl BinaryFuncInfo {
-    fn call(&self, context: &mut interpreter::Context, x: f64, y: f64, angle_unit: &str) -> f64 {
-        let func = self.0;
-        match self.1 {
-            FuncType::Trig => func(
-                from_angle_unit(context, x, angle_unit),
-                from_angle_unit(context, y, angle_unit),
-            ),
-            FuncType::InverseTrig => to_angle_unit(context, func(x, y), angle_unit),
-            FuncType::Other => func(x, y),
-        }
-    }
-}
-
-pub fn is_prelude_func(identifier: &str) -> bool {
-    identifier == "sum"
-        || identifier == "Σ"
-        || identifier == "integrate"
-        || identifier == "∫"
-        || UNARY_FUNCS.contains_key(identifier)
-        || BINARY_FUNCS.contains_key(identifier)
-}
-
-pub fn call_unary_func(
-    context: &mut interpreter::Context,
-    name: &str,
-    x: f64,
-    angle_unit: &str,
-) -> Option<(f64, String)> {
-    if let Some((func_info, func_unit)) = UNARY_FUNCS.get(name) {
-        Some((
-            func_info.call(context, x, &angle_unit),
-            func_unit.to_string(),
-        ))
-    } else {
-        None
-    }
-}
-
-pub fn call_binary_func(
-    context: &mut interpreter::Context,
-    name: &str,
-    x: f64,
-    y: f64,
-    angle_unit: &str,
-) -> Option<(f64, String)> {
-    if let Some((func_info, func_unit)) = BINARY_FUNCS.get(name) {
-        Some((
-            func_info.call(context, x, y, angle_unit),
-            func_unit.to_string(),
-        ))
-    } else {
-        None
-    }
-}
-
-fn to_angle_unit(context: &mut interpreter::Context, x: f64, angle_unit: &str) -> f64 {
-    match angle_unit {
-        "rad" => x,
-        _ => {
-            interpreter::convert_unit(context, &Expr::Literal(x), "rad", angle_unit)
-                .unwrap()
-                .value
-        }
-    }
-}
-
-fn from_angle_unit(context: &mut interpreter::Context, x: f64, angle_unit: &str) -> f64 {
-    match angle_unit {
-        "rad" => x,
-        _ => {
-            interpreter::convert_unit(context, &Expr::Literal(x), angle_unit, "rad")
-                .unwrap()
-                .value
-        }
-    }
-}
-
 pub mod special_funcs {
-    pub fn factorial(x: f64) -> f64 {
-        super::funcs::gamma(x + 1f64)
+    use crate::kalk_num::KalkNum;
+
+    pub fn factorial(x: KalkNum) -> KalkNum {
+        // Round it a bit, to prevent floating point errors.
+        KalkNum::new(
+            (super::funcs::precise_gamma(x.value + 1f64) * 10e6f64).round() / 10e6f64,
+            &x.unit,
+        )
     }
 }
 
-pub(super) mod funcs {
-    pub fn abs(x: f64) -> f64 {
-        x.abs()
+pub(crate) mod funcs {
+    use crate::kalk_num::KalkNum;
+    use crate::prelude::funcs::abs;
+
+    pub fn arg(x: KalkNum) -> KalkNum {
+        // i(ln|x| - ln(x))
+        KalkNum::new(x.imaginary_value.atan2(x.value), &x.unit)
     }
 
-    pub fn acos(x: f64) -> f64 {
-        x.acos()
-    }
-
-    pub fn acosh(x: f64) -> f64 {
-        x.acosh()
-    }
-
-    pub fn acot(x: f64) -> f64 {
-        (1f64 / x).atan()
-    }
-
-    pub fn acoth(x: f64) -> f64 {
-        (1f64 / x).atanh()
-    }
-
-    pub fn acosec(x: f64) -> f64 {
-        (1f64 / x).asin()
-    }
-
-    pub fn acosech(x: f64) -> f64 {
-        (1f64 / x).asinh()
-    }
-
-    pub fn asec(x: f64) -> f64 {
-        (1f64 / x).acos()
-    }
-
-    pub fn asech(x: f64) -> f64 {
-        (1f64 / x).acosh()
-    }
-
-    pub fn asin(x: f64) -> f64 {
-        x.asin()
-    }
-
-    pub fn asinh(x: f64) -> f64 {
-        x.asinh()
-    }
-
-    pub fn atan(x: f64) -> f64 {
-        x.atan()
-    }
-
-    pub fn atanh(x: f64) -> f64 {
-        x.atanh()
-    }
-
-    pub fn cbrt(x: f64) -> f64 {
-        x.cbrt()
-    }
-
-    pub fn ceil(x: f64) -> f64 {
-        x.ceil()
-    }
-
-    pub fn cos(x: f64) -> f64 {
-        x.cos()
-    }
-
-    pub fn cosh(x: f64) -> f64 {
-        x.cos()
-    }
-
-    pub fn cosec(x: f64) -> f64 {
-        1f64 / x.sin()
-    }
-
-    pub fn cosech(x: f64) -> f64 {
-        1f64 / x.sinh()
-    }
-
-    pub fn cot(x: f64) -> f64 {
-        x.clone().cos() / x.sin()
-    }
-
-    pub fn coth(x: f64) -> f64 {
-        x.clone().cosh() / x.sinh()
-    }
-
-    pub fn exp(x: f64) -> f64 {
-        x.exp()
-    }
-
-    pub fn floor(x: f64) -> f64 {
-        x.floor()
-    }
-
-    pub fn frac(x: f64) -> f64 {
-        x.fract()
-    }
-
-    pub fn gamma(x: f64) -> f64 {
+    pub fn gamma(x: KalkNum) -> KalkNum {
         // Round it a bit, to prevent floating point errors.
-        (precise_gamma(x) * 10e6f64).round() / 10e6f64
+        KalkNum::new(
+            (precise_gamma(x.value) * 10e6f64).round() / 10e6f64,
+            &x.unit,
+        )
     }
 
     // Matthias Eiholzer - https://gitlab.com/matthiaseiholzer/mathru/-/tree/master
-    fn precise_gamma(x: f64) -> f64 {
+    pub(super) fn precise_gamma(x: f64) -> f64 {
         let pi = 3.1415926535897932384626433832795028841971693993751058209749445923f64;
         if x == 0f64 {
             return f64::NAN;
@@ -227,67 +50,26 @@ pub(super) mod funcs {
         2f64.sqrt() * pi.sqrt() * t.powf(x - 0.5f64) * (-t).exp() * a
     }
 
-    pub fn hyp(x: f64, y: f64) -> f64 {
-        x.hypot(y)
+    pub fn hypot(x: KalkNum, y: KalkNum) -> KalkNum {
+        if x.has_imaginary() || y.has_imaginary() {
+            let abs_x = abs(x);
+            let abs_y = abs(y);
+            crate::prelude::funcs::sqrt(
+                abs_x
+                    .clone()
+                    .mul_without_unit(abs_x)
+                    .add_without_unit(abs_y.clone().mul_without_unit(abs_y)),
+            )
+        } else {
+            KalkNum::new(x.value.hypot(y.value), &x.unit)
+        }
     }
 
-    pub fn log(x: f64) -> f64 {
-        x.log10()
+    pub fn max(x: KalkNum, y: KalkNum) -> KalkNum {
+        KalkNum::new(x.value.max(y.value), &x.unit)
     }
 
-    pub fn logx(x: f64, y: f64) -> f64 {
-        x.log10() / y.log10()
-    }
-
-    pub fn ln(x: f64) -> f64 {
-        x.ln()
-    }
-
-    pub fn max(x: f64, y: f64) -> f64 {
-        x.max(y)
-    }
-
-    pub fn min(x: f64, y: f64) -> f64 {
-        x.min(y)
-    }
-
-    pub fn round(x: f64) -> f64 {
-        x.round()
-    }
-
-    pub fn sec(x: f64) -> f64 {
-        1f64 / x.cos()
-    }
-
-    pub fn sech(x: f64) -> f64 {
-        1f64 / x.cosh()
-    }
-
-    pub fn sin(x: f64) -> f64 {
-        x.sin()
-    }
-
-    pub fn sinh(x: f64) -> f64 {
-        x.sinh()
-    }
-
-    pub fn sqrt(x: f64) -> f64 {
-        x.sqrt()
-    }
-
-    pub fn nth_root(x: f64, n: f64) -> f64 {
-        x.powf(1f64 / n)
-    }
-
-    pub fn tan(x: f64) -> f64 {
-        x.tan()
-    }
-
-    pub fn tanh(x: f64) -> f64 {
-        x.tanh()
-    }
-
-    pub fn trunc(x: f64) -> f64 {
-        x.trunc()
+    pub fn min(x: KalkNum, y: KalkNum) -> KalkNum {
+        KalkNum::new(x.value.min(y.value), &x.unit)
     }
 }
--- kalk/src/prelude/with_rug.rs
+++ kalk/src/prelude/with_rug.rs
@@ -1,291 +1,44 @@
-use super::*;
-use crate::ast::Expr;
-use crate::interpreter;
-use rug::Float;
-
-// Unary functions
-pub struct UnaryFuncInfo(pub(super) fn(Float) -> Float, pub(super) FuncType);
-
-pub struct BinaryFuncInfo(pub(super) fn(Float, Float) -> Float, pub(super) FuncType);
-
-impl UnaryFuncInfo {
-    fn call(&self, context: &mut interpreter::Context, x: Float, angle_unit: &str) -> Float {
-        let func = self.0;
-        match self.1 {
-            FuncType::Trig => func(from_angle_unit(context, x, angle_unit)),
-            FuncType::InverseTrig => to_angle_unit(context, func(x), angle_unit),
-            FuncType::Other => func(x),
-        }
-    }
-}
-
-impl BinaryFuncInfo {
-    fn call(
-        &self,
-        context: &mut interpreter::Context,
-        x: Float,
-        y: Float,
-        angle_unit: &str,
-    ) -> Float {
-        let func = self.0;
-        match self.1 {
-            FuncType::Trig => func(
-                from_angle_unit(context, x, angle_unit),
-                from_angle_unit(context, y, angle_unit),
-            ),
-            FuncType::InverseTrig => to_angle_unit(context, func(x, y), angle_unit),
-            FuncType::Other => func(x, y),
-        }
-    }
-}
-
-pub fn is_prelude_func(identifier: &str) -> bool {
-    identifier == "sum"
-        || identifier == "Σ"
-        || identifier == "integrate"
-        || identifier == "∫"
-        || UNARY_FUNCS.contains_key(identifier)
-        || BINARY_FUNCS.contains_key(identifier)
-}
-
-pub fn call_unary_func(
-    context: &mut interpreter::Context,
-    name: &str,
-    x: Float,
-    angle_unit: &str,
-) -> Option<(Float, String)> {
-    if let Some((func_info, func_unit)) = UNARY_FUNCS.get(name) {
-        Some((
-            func_info.call(context, x, &angle_unit),
-            func_unit.to_string(),
-        ))
-    } else {
-        None
-    }
-}
-
-pub fn call_binary_func(
-    context: &mut interpreter::Context,
-    name: &str,
-    x: Float,
-    y: Float,
-    angle_unit: &str,
-) -> Option<(Float, String)> {
-    if let Some((func_info, func_unit)) = BINARY_FUNCS.get(name) {
-        Some((
-            func_info.call(context, x, y, angle_unit),
-            func_unit.to_string(),
-        ))
-    } else {
-        None
-    }
-}
-
-fn to_angle_unit(context: &mut interpreter::Context, x: Float, angle_unit: &str) -> Float {
-    match angle_unit {
-        "rad" => x,
-        _ => {
-            interpreter::convert_unit(
-                context,
-                &Expr::Literal(x.to_f64_round(rug::float::Round::Nearest)),
-                "rad",
-                angle_unit,
-            )
-            .unwrap()
-            .value
-        }
-    }
-}
-
-fn from_angle_unit(context: &mut interpreter::Context, x: Float, angle_unit: &str) -> Float {
-    match angle_unit {
-        "rad" => x,
-        _ => {
-            interpreter::convert_unit(
-                context,
-                &Expr::Literal(x.to_f64_round(rug::float::Round::Nearest)),
-                angle_unit,
-                "rad",
-            )
-            .unwrap()
-            .value
-        }
-    }
-}
-
 pub mod special_funcs {
-    use rug::Float;
+    use crate::prelude::KalkNum;
 
-    pub fn factorial(x: Float) -> Float {
-        ((x + 1) as Float).gamma()
+    pub fn factorial(x: KalkNum) -> KalkNum {
+        KalkNum::new((x.value + 1f64).gamma(), &x.unit)
     }
 }
 
-pub(super) mod funcs {
-    use rug::ops::Pow;
-    use rug::Float;
+pub(crate) mod funcs {
+    use crate::kalk_num::KalkNum;
+    use crate::prelude::funcs::abs;
 
-    pub fn abs(x: Float) -> Float {
-        x.abs()
+    pub fn arg(x: KalkNum) -> KalkNum {
+        // i(ln|x| - ln(x))
+        KalkNum::new(x.imaginary_value.atan2(&x.value), &x.unit)
     }
 
-    pub fn acos(x: Float) -> Float {
-        x.acos()
+    pub fn gamma(x: KalkNum) -> KalkNum {
+        KalkNum::new(x.value.gamma(), &x.unit)
     }
 
-    pub fn acosh(x: Float) -> Float {
-        x.acosh()
+    pub fn hypot(x: KalkNum, y: KalkNum) -> KalkNum {
+        if x.has_imaginary() || y.has_imaginary() {
+            let abs_x = abs(x);
+            let abs_y = abs(y);
+            crate::prelude::funcs::sqrt(
+                abs_x
+                    .clone()
+                    .mul_without_unit(abs_x)
+                    .add_without_unit(abs_y.clone().mul_without_unit(abs_y)),
+            )
+        } else {
+            KalkNum::new(x.value.hypot(&y.value), &x.unit)
+        }
     }
 
-    pub fn acot(x: Float) -> Float {
-        (1f64 / x).atan()
+    pub fn max(x: KalkNum, y: KalkNum) -> KalkNum {
+        KalkNum::new(x.value.max(&y.value), &x.unit)
     }
 
-    pub fn acoth(x: Float) -> Float {
-        (1f64 / x).atanh()
-    }
-
-    pub fn acosec(x: Float) -> Float {
-        (1f64 / x).asin()
-    }
-
-    pub fn acosech(x: Float) -> Float {
-        (1f64 / x).asinh()
-    }
-
-    pub fn asec(x: Float) -> Float {
-        (1f64 / x).acos()
-    }
-
-    pub fn asech(x: Float) -> Float {
-        (1f64 / x).acosh()
-    }
-
-    pub fn asin(x: Float) -> Float {
-        x.asin()
-    }
-
-    pub fn asinh(x: Float) -> Float {
-        x.asinh()
-    }
-
-    pub fn atan(x: Float) -> Float {
-        x.atan()
-    }
-
-    pub fn atanh(x: Float) -> Float {
-        x.atanh()
-    }
-
-    pub fn cbrt(x: Float) -> Float {
-        x.cbrt()
-    }
-
-    pub fn ceil(x: Float) -> Float {
-        x.ceil()
-    }
-
-    pub fn cos(x: Float) -> Float {
-        x.cos()
-    }
-
-    pub fn cosh(x: Float) -> Float {
-        x.cos()
-    }
-
-    pub fn cosec(x: Float) -> Float {
-        1f64 / x.sin()
-    }
-
-    pub fn cosech(x: Float) -> Float {
-        1f64 / x.sinh()
-    }
-
-    pub fn cot(x: Float) -> Float {
-        x.clone().cos() / x.sin()
-    }
-
-    pub fn coth(x: Float) -> Float {
-        x.clone().cosh() / x.sinh()
-    }
-
-    pub fn exp(x: Float) -> Float {
-        x.exp()
-    }
-
-    pub fn floor(x: Float) -> Float {
-        x.floor()
-    }
-
-    pub fn frac(x: Float) -> Float {
-        x.fract()
-    }
-
-    pub fn gamma(x: Float) -> Float {
-        x.gamma()
-    }
-
-    pub fn hyp(x: Float, y: Float) -> Float {
-        x.hypot(&y)
-    }
-
-    pub fn log(x: Float) -> Float {
-        x.log10()
-    }
-
-    pub fn logx(x: Float, y: Float) -> Float {
-        x.log10() / y.log10()
-    }
-
-    pub fn ln(x: Float) -> Float {
-        x.ln()
-    }
-
-    pub fn max(x: Float, y: Float) -> Float {
-        x.max(&y)
-    }
-
-    pub fn min(x: Float, y: Float) -> Float {
-        x.min(&y)
-    }
-
-    pub fn round(x: Float) -> Float {
-        x.round()
-    }
-
-    pub fn sec(x: Float) -> Float {
-        1f64 / x.cos()
-    }
-
-    pub fn sech(x: Float) -> Float {
-        1f64 / x.cosh()
-    }
-
-    pub fn sin(x: Float) -> Float {
-        x.sin()
-    }
-
-    pub fn sinh(x: Float) -> Float {
-        x.sinh()
-    }
-
-    pub fn sqrt(x: Float) -> Float {
-        x.sqrt()
-    }
-
-    pub fn nth_root(x: Float, n: Float) -> Float {
-        x.pow(Float::with_val(1, 1) / n)
-    }
-
-    pub fn tan(x: Float) -> Float {
-        x.tan()
-    }
-
-    pub fn tanh(x: Float) -> Float {
-        x.tanh()
-    }
-
-    pub fn trunc(x: Float) -> Float {
-        x.trunc()
+    pub fn min(x: KalkNum, y: KalkNum) -> KalkNum {
+        KalkNum::new(x.value.min(&y.value), &x.unit)
     }
 }
