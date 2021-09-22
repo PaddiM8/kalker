d2417b2f7ee8038364952472a6ada20cd749a2f9
PaddiM8 <paddim8@pm.me>
2021-09-22 16:23:10 +0200

    Output numbers with the precision specified

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -51,7 +51,9 @@ fn default_action(context: &Context) {
     let mut parser_context = parser::Context::new()
         .set_angle_unit(&angle_unit)
         .set_timeout(None);
-    let precision = context.int_flag("precision").unwrap_or(53isize) as u32;
+    let precision = context
+        .int_flag("precision")
+        .unwrap_or(output::DEFAULT_PRECISION as isize) as u32;
 
     if let Ok(input_file_path) = context.string_flag("input-file") {
         load_input_file(&input_file_path, precision, &mut parser_context);
--- cli/src/output.rs
+++ cli/src/output.rs
@@ -1,9 +1,17 @@
 use ansi_term::Colour::Red;
 use kalk::parser;
 
+pub(crate) const DEFAULT_PRECISION: u32 = 63;
+
 pub fn eval(parser: &mut parser::Context, input: &str, precision: u32) {
     match parser::eval(parser, input, precision) {
-        Ok(Some(result)) => println!("{}", result.to_string_pretty()),
+        Ok(Some(result)) => {
+            if precision == DEFAULT_PRECISION {
+                println!("{}", result.to_string_pretty())
+            } else {
+                println!("{}", result.to_string_big())
+            }
+        }
         Ok(None) => print!(""),
         Err(err) => print_err(&err.to_string()),
     }
