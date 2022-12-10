524999493c8999820bb302c6bcfdbd43a3b3fa25
PaddiM8 <bakk@tuta.io>
2022-12-10 02:21:54 +0100

    mobile: Use local font file

--- mobile/remove-proprietary.sh
+++ mobile/remove-proprietary.sh
@@ -7,9 +7,9 @@ sed -i '/playServices/d' "$dir/build.gradle"
 sed -i '/firebase/d' "$dir/build.gradle"
 
 # remove the files that rely on the proprietary services
-rm "$dir/src/main/java/com/getcapacitor/plugin/Geolocation.java"
-rm "$dir/src/main/java/com/getcapacitor/plugin/PushNotifications.java"
-rm "$dir/src/main/java/com/getcapacitor/CapacitorFirebaseMessagingService.java"
+rm -f "$dir/src/main/java/com/getcapacitor/plugin/Geolocation.java"
+rm -f "$dir/src/main/java/com/getcapacitor/plugin/PushNotifications.java"
+rm -f "$dir/src/main/java/com/getcapacitor/CapacitorFirebaseMessagingService.java"
 
 # remove the references to the plugins in Bridge.java
 sed -i '/Geolocation/d' "$dir/src/main/java/com/getcapacitor/Bridge.java"
@@ -19,4 +19,4 @@ sed -i '/PushNotifications/d' "$dir/src/main/java/com/getcapacitor/Bridge.java"
 sed -i '/WAKE_LOCK/d' "$dir/src/main/AndroidManifest.xml"
 sed -i '/RECEIVE_BOOT_COMPLETED/d' "$dir/src/main/AndroidManifest.xml"
 i=$(grep -n "firebase_messaging_auto_init_enabled" "$dir/src/main/AndroidManifest.xml" | cut -d : -f 1)
-sed -i "$i,$(($i+12))d" "$dir/src/main/AndroidManifest.xml"
+sed -i "$i,$(($i+12))d" "$dir/src/main/AndroidManifest.xml" 2> /dev/null
